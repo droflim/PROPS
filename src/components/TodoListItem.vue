ce9a571a4805f228ccb2354a7b4cfa9e05f89950
droflim <estay.milford@gmail.com>
2021-07-29 19:41:42 -0400

    Uploaded Files

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <div v-if="!tarea.modificable">{{ tarea.$index }}: {{ tarea.texto }}</div><button @click="borrar(tarea)">Eliminar</button>
+    
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: {
+    tarea: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    borrar(tarea) {
+      this.$emit('borrar-tarea', tarea)
+    },
+  },
+}
+</script>
+
+<style></style>
